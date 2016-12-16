23c1f72516789fff5aa19a777df3c37277b794d2
monty-georgiev <monty.georgiev@gmail.com>
2016-12-16 15:46:07 +0200

    Full board
    Game should end if the board is full;

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,19 +1,18 @@
 
-
 board = [' '] * 10
 player_turn = 'Player 1'
 player_one_turn = True
 winner = False
 
-def user_input():
+def user_input(board):
     # ask for user input
 
+   
     global player_turn
     global player_one_turn
     player_position = input(player_turn + ' choose position (1-9):')
-    place_mark_on_board(player_turn, player_position)
+    place_mark_on_board(board,player_turn, player_position)
 
-     # change players on input
     if player_one_turn:
         player_turn = 'Player 2'
         player_one_turn = False
@@ -21,9 +20,10 @@ def user_input():
         player_turn = 'Player 1'
         player_one_turn = True
 
-def draw_board():
-    global board
+def draw_board(board):
  
+    print(chr(27) + "[2J")
+
     print ' ___.___.___'
     print '|   |   |   |'    
     print '| {} | {} | {} |'.format(board[1], board[2], board[3])
@@ -35,11 +35,17 @@ def draw_board():
     print '| {} | {} | {} |'.format(board[7], board[8], board[9])
     print '|___|___|___|'
 
+def check_if_board_full(board):
+    
+    for field in xrange(1,10):
+        if board[field] == ' ':
+            return False
+    return True     
 
-def place_mark_on_board(player_turn, player_position):
+
+def place_mark_on_board(board, player_turn, player_position):
     print '{} placed mark on position: {}'.format(player_turn, player_position)
     marker = 'X'
-    global board
     global player_one_turn
 
     if not player_one_turn:
@@ -50,12 +56,12 @@ def place_mark_on_board(player_turn, player_position):
     else:
         print 'Position already taken!'
 
-    draw_board()
-    check_for_winner(player_turn, marker)
+    draw_board(board)
+    check_for_winner(board, player_turn, marker)
 
 
-def check_for_winner(player_turn, marker):
-    global board
+def check_for_winner(board, player_turn, marker):
+
     global winner
 
     if ((board[1] == marker and board[2] == marker and board[3] == marker) or
@@ -69,6 +75,8 @@ def check_for_winner(player_turn, marker):
         winner = True
         print '{} is the winner'.format(player_turn)
 
-while not winner:
-    user_input()
+while not winner and not check_if_board_full(board):
+    user_input(board)
+else:
+     print 'Game Over!'
     

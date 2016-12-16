db5d113f4b7dbdf213bc88b9042a5a73150fd311
monty-georgiev <monty.georgiev@gmail.com>
2016-12-16 12:19:41 +0200

    Draw
    Basic drawing

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,6 @@
 
 
-board = []
+board = [' ']*10
 player_turn = 'Player 1'
 
 
@@ -14,15 +14,22 @@ def user_input():
 
     place_mark_on_board(player_turn, player_position)
   # change players on input
-      if player_turn == 'Player 1':
-            player_turn = 'Player 2'
+    if player_turn == 'Player 1':
+        player_turn = 'Player 2'
     else:
         player_turn = 'Player 1'
 
 
 def draw_board():
+    global board
     # get all positions and draw the board
-    pass
+    print '---|---|---'
+    print ' {} | {} | {} '.format(board[1], board[2], board[3])
+    print '---|---|---'
+    print ' {} | {} | {} '.format(board[4], board[5], board[6])
+    print '---|---|---'
+    print ' {} | {} | {} '.format(board[7], board[8], board[9])
+    print '---|---|---'
 
 
 def place_mark_on_board(player_turn, player_position):
@@ -31,6 +38,9 @@ def place_mark_on_board(player_turn, player_position):
     # if not -> return error
     # update state of board
     print '{} placed mark on position: {}'.format(player_turn, player_position)
+    global board
+    board[player_position] = 'M'
+    draw_board()
 
 
 user_input()

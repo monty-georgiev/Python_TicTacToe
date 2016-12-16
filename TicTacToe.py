3d9ba2145ed995363769ee588adf5a5ebfb798a0
monty-georgiev <monty.georgiev@gmail.com>
2016-12-16 13:44:02 +0200

    Main functionality working; TODO: reformat code; docstrings and remove global use?

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,30 +1,29 @@
 
 
-board = [' ']*10
+board = [' '] * 10
 player_turn = 'Player 1'
 player_one_turn = True
 winner = False
 
-
 def user_input():
     # ask for user input
-  
+
     global player_turn
     global player_one_turn
     player_position = input(player_turn + ' choose position (1-9):')
     place_mark_on_board(player_turn, player_position)
-  
+
   # change players on input
     if player_one_turn:
         player_turn = 'Player 2'
         player_one_turn = False
     else:
         player_turn = 'Player 1'
-        player_one_turn = True        
+        player_one_turn = True
 
 def draw_board():
     global board
-    # get all positions and draw the board
+ 
     print '---|---|---'
     print ' {} | {} | {} '.format(board[1], board[2], board[3])
     print '---|---|---'
@@ -35,10 +34,6 @@ def draw_board():
 
 
 def place_mark_on_board(player_turn, player_position):
-    # check if position available
-    # if yes -> place mark
-    # if not -> return error
-    # update state of board
     print '{} placed mark on position: {}'.format(player_turn, player_position)
     marker = 'X'
     global board
@@ -46,10 +41,27 @@ def place_mark_on_board(player_turn, player_position):
 
     if not player_one_turn:
         marker = 'O'
-        
+
     board[player_position] = marker
     draw_board()
+    check_for_winner(player_turn, marker)
 
 
-for i in range(3):
+def check_for_winner(player_turn, marker):
+    global board
+    global winner
+
+    if ((board[1] == marker and board[2] == marker and board[3] == marker) or
+        (board[4] == marker and board[5] == marker and board[6] == marker) or
+        (board[7] == marker and board[8] == marker and board[9] == marker) or
+        (board[1] == marker and board[4] == marker and board[7] == marker) or
+        (board[2] == marker and board[5] == marker and board[8] == marker) or
+        (board[3] == marker and board[6] == marker and board[9] == marker) or
+        (board[1] == marker and board[5] == marker and board[9] == marker) or
+            (board[3] == marker and board[5] == marker and board[7] == marker)):
+        winner = True
+        print '{} is the winner'.format(player_turn)
+
+while not winner:
     user_input()
+    
